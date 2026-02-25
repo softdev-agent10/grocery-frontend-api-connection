4d080a1cd5d1fc711bfb45bcb58fa98a570058a9
softdev-agent03 <oss.softdev.agent03@gmail.com>
2026-02-26 01:32:56 +0600

    Initial commit by Sharif

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <p>This is POS</p>
+    </div>
+  );
+}
